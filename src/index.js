f052a789c804a7554e9491098216a25d0f057bea
bernabasy <104406349+bernabasy@users.noreply.github.com>
2022-12-14 10:14:01 +0300

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import _ from "lodash";
-import "./style.css";
-import Icon from "./image/icon.png";
-import arrow from "./image/arrow.png";
-import more from "./image/more.png";
-const icon1 = document.querySelector("#icon");
+import './style.css';
+import Icon from './image/icon.png';
+import arrow from './image/arrow.png';
+import more from './image/more.png';
+
+const icon1 = document.querySelector('#icon');
 icon1.src = Icon;
-const arrowimg = document.querySelector("#arrowimg");
+const arrowimg = document.querySelector('#arrowimg');
 arrowimg.src = arrow;
 
-const todoList = document.querySelector("#todo-list-wrap");
+const todoList = document.querySelector('#todo-list-wrap');
 const taskLists = [
   {
-    description: "Build a To Do List App",
+    description: 'Build a To Do List App',
     completed: false,
     index: 0,
   },
   {
-    description: "Learn Javascript",
+    description: 'Learn Javascript',
     completed: false,
     index: 1,
   },
   {
-    description: "read",
+    description: 'read',
     completed: false,
     index: 2,
   },
@@ -29,28 +29,28 @@ const taskLists = [
 
 const display = () => {
   taskLists.forEach((todo) => {
-    const todoItem = document.createElement("div");
-    todoItem.classList.add("todo-item");
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('todo-item');
 
-    const label = document.createElement("div");
-    const input = document.createElement("input");
-    const span = document.createElement("span");
-    const content = document.createElement("div");
-    const actions = document.createElement("div");
-    const editImg = document.createElement("img");
+    const label = document.createElement('div');
+    const input = document.createElement('input');
+    const span = document.createElement('span');
+    const content = document.createElement('div');
+    const actions = document.createElement('div');
+    const editImg = document.createElement('img');
 
-    input.type = "checkbox";
+    input.type = 'checkbox';
     input.checked = todo.completed;
-    input.setAttribute("aria-label", "checkbox");
+    input.setAttribute('aria-label', 'checkbox');
     editImg.src = more;
-    editImg.setAttribute("alt", "restart todo");
+    editImg.setAttribute('alt', 'restart todo');
 
-    label.classList.add("label");
-    span.classList.add("bubble");
-    content.classList.add("todo-content");
-    actions.classList.add("actions");
+    label.classList.add('label');
+    span.classList.add('bubble');
+    content.classList.add('todo-content');
+    actions.classList.add('actions');
 
-    content.innerHTML = `<input type="text" value="${todo.description}" aria-label="task" readonly>`;
+    content.innerHTML = `<input type='text' value='${todo.description}' aria-label='task' readonly>`;
 
     todoList.appendChild(todoItem);
     todoItem.appendChild(label);
