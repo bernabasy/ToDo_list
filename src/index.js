95b07a9bb596cc354cafa2920ebc7c167cf1c648
bernabasy <104406349+bernabasy@users.noreply.github.com>
2022-12-14 18:09:00 +0300

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -33,4 +33,3 @@ window.addEventListener('load', () => {
 
   display();
 });
-
--- src/modules/Remove.js
+++ src/modules/Remove.js
@@ -1,11 +1,11 @@
 const setIds = () => {
-    const todos = JSON.parse(localStorage.getItem('todos')) || [];
-    todos.forEach((todo, index) => {
-      todo.id = index + 1;
-    });
-    localStorage.setItem('todos', JSON.stringify(todos));
-  };
-  
+  const todos = JSON.parse(localStorage.getItem('todos')) || [];
+  todos.forEach((todo, index) => {
+    todo.id = index + 1;
+  });
+  localStorage.setItem('todos', JSON.stringify(todos));
+};
+
 const Remove = {
   todo(id) {
     const todos = JSON.parse(localStorage.getItem('todos')) || [];
