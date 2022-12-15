eb8faa9afabaeef93aa41b5ed9e7fbf32e2276d1
bernabasy <104406349+bernabasy@users.noreply.github.com>
2022-12-15 15:21:52 +0300

    Update index.js to separet add function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 import display from './modules/toDo_UI.js';
 import Icon from './image/icon.png';
 import arrow from './image/arrow.png';
+import addToDo from './modules/add.js';
 
 const icon1 = document.querySelector('#icon');
 icon1.src = Icon;
@@ -9,27 +10,12 @@ const arrowimg = document.querySelector('#arrowimg');
 arrowimg.src = arrow;
 
 window.addEventListener('load', () => {
-  const todos = JSON.parse(localStorage.getItem('todos')) || [];
   const newTodoForm = document.querySelector('#new-todo-form');
-
   newTodoForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    const todo = {
-      content: e.target.elements.content.value,
-      complet: false,
-      id: todos.length + 1,
-    };
-
-    // Global variable = todos
-    todos.push(todo);
-
-    localStorage.setItem('todos', JSON.stringify(todos));
-
+    addToDo(e);
     e.target.reset();
-
     display();
   });
-
   display();
 });
