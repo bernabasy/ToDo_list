6b56b42623cf28794e874be6e484987c97205fe1
bernabasy <104406349+bernabasy@users.noreply.github.com>
2022-12-16 00:26:05 +0300

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import "./style.css";
-import display from "./modules/toDo_UI.js";
-import Icon from "./image/icon.png";
-import arrow from "./image/arrow.png";
-import addToDo from "./modules/add.js";
+import './style.css';
+import display from './modules/toDo_UI.js';
+import Icon from './image/icon.png';
+import arrow from './image/arrow.png';
+import addToDo from './modules/add.js';
 
-const icon1 = document.querySelector("#icon");
+const icon1 = document.querySelector('#icon');
 icon1.src = Icon;
-const arrowimg = document.querySelector("#arrowimg");
+const arrowimg = document.querySelector('#arrowimg');
 arrowimg.src = arrow;
 
-window.addEventListener("load", () => {
-  const newTodoForm = document.querySelector("#new-todo-form");
-  newTodoForm.addEventListener("submit", (e) => {
+window.addEventListener('load', () => {
+  const newTodoForm = document.querySelector('#new-todo-form');
+  newTodoForm.addEventListener('submit', (e) => {
     e.preventDefault();
     addToDo(e);
     e.target.reset();
